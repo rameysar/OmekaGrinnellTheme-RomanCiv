6994117c9cc2069c1ab04a63d5effb9b17d791a4
DigitalGrinnell <digital@grinnell.edu>
2014-10-27 17:32:48 -0500

    First local commit before push to GitHub.

---
+++ common/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php if ($description = option('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>">
+    <?php endif; ?>
+
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+
+    <?php echo auto_discovery_link_tags(); ?>
+
+    <!-- Plugin Stuff -->
+    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+
+    <!-- Stylesheets -->
+    <?php
+    queue_css_url('//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic|Droid+Serif:400,700');
+    queue_css_file('normalize');
+    queue_css_file('style', 'screen');
+    queue_css_file('print', 'print');
+    echo head_css();
+    ?>
+
+    <!-- JavaScripts -->
+    <?php queue_js_file('vendor/modernizr'); ?>
+    <?php queue_js_file('vendor/selectivizr'); ?>
+    <?php queue_js_file('jquery-extra-selectors'); ?>
+    <?php queue_js_file('vendor/respond'); ?>
+    <?php queue_js_file('globals'); ?>
+    <?php echo head_js(); ?>
+</head>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
+    <div id="wrap">
+        <header>
+            <div id="site-title">
+		<div id="grinnell-branding"><a id="ext-gen1138" class="grinnell-home" title="Grinnell College" href="http://www.grinnell.edu/"><img src="https://libweb.grinnell.edu/sp/assets/images/sitetheme_logo.png" alt="Grinnell College" /></a></div>
+                <?php echo link_to_home_page(theme_logo()); ?>
+            </div>
+            <div id="search-container">
+                <?php echo search_form(array('show_advanced' => true)); ?>
+            </div>
+            <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
+        </header>
+
+        <nav class="top">
+            <?php echo public_nav_main(); ?>
+        </nav>
+
+        <div id="content">
+            <?php
+                if(! is_current_url(WEB_ROOT)) {
+                  fire_plugin_hook('public_content_top', array('view'=>$this));
+                }
+            ?>
